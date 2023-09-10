d99f6c9581d69056d1f5dcf4407a1a70821e281b
Ayush Parui <paruiayush900@gmail.com>
2023-09-10 03:00:36 -0700

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,7 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
-    // token: "BQB0REa4O6VN8jd7cHsXDgwXzM4a1fJ8kY7x9XVZoFQKzW4B5qTV7_JPz5e_uEPTQyJw7Gf6CFv7atjSdwDW2sYr9mibLpNhKOFdq_PS5zVgrbXwc3AablyhvnxBNEpSSYtQS5_e6LSdxZaqX2uyfRS9wCT4i1MjGdc_OPiYre3QsieXfGLTASWux6ZXqx_E71ryXX4zhnjdqLbO",
+    token: "BQB0REa4O6VN8jd7cHsXDgwXzM4a1fJ8kY7x9XVZoFQKzW4B5qTV7_JPz5e_uEPTQyJw7Gf6CFv7atjSdwDW2sYr9mibLpNhKOFdq_PS5zVgrbXwc3AablyhvnxBNEpSSYtQS5_e6LSdxZaqX2uyfRS9wCT4i1MjGdc_OPiYre3QsieXfGLTASWux6ZXqx_E71ryXX4zhnjdqLbO",
 }
 
 const reducer = (state, action) => {
